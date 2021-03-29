b68c5c30131f6ba2457caf00f74d0e1e7c6099a2
yasum <yasum@192.168.1.8>
2021-03-28 20:48:40 -0500

    Casos guia2

---
+++ src/com/senati/eti/Caso4.java
@@ -0,0 +1,22 @@
+package com.senati.eti;
+import java.util.Scanner;
+public class Caso4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Ingrese primer número: ");
+		int n1 = sc.nextInt();
+		String num ="";
+		System.out.println("Ingrese segundo número: ");
+	    int n2 = sc.nextInt();
+
+		if(n1==n2)
+		num=("Son iguales:");
+	    if (n1>n2)
+		if (n1>n2)
+		num=("Es menor: "+n1);
+	    System.out.println(num);
+	}
+
+}
